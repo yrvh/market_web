2bdcc22c9584ef714341d69d95f2cddc8bd32164
Guoxh <AGuoxh@163.com>
2019-12-13 17:59:08 +0800

    首页和分类页的样式, 组件拆分

---
+++ components/goods-list/goods-list.js
@@ -0,0 +1,31 @@
+// components/goods-list/goods-list.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    imgUrl: {
+      type: String,
+    },
+    goodsText: {
+      type: String,
+    },
+    goodsPrice: {
+      type: String,
+    },
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
---
+++ components/yellow-btn/yellow-btn.js
@@ -0,0 +1,32 @@
+// components/yellow-btn/yellow-btn.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    title: {
+      type: String,
+      value: "特别推荐"
+    },
+    active: {
+      type: Number,
+      value: 0
+    }
+  },
+
+
+  externalClasses: ['btn-width'],
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
